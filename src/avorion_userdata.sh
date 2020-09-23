2e373c66d1fcd6cf1eac4e0a4e2a67e07266d84c
jaredzieche <root@DESKTOP-PSFMCKA.localdomain>
2020-09-22 21:46:05 -0400

    added avorio user data

---
+++ src/avorion_userdata.sh
@@ -0,0 +1,26 @@
+# append this to advanced user data options #
+!#/bin/bash
+yum update -y
+yum install glibc.i686 libstdc++.i686
+useradd -m steam
+su - steam
+mkdir ~/Steam && cd ~/Steam
+wget http://media.steampowered.com/installer/steamcmd_linux.tar.gz
+tar -xvzf steamcmd_linux.tar.gz
+./steamcmd.sh +login anonymous +force_install_dir ./avorion/ +app_update 565060 validate +exit
+cd avorion
+mkdir galaxies
+
+# run manually after instance is initialized #
+#create galaxy #
+./server.sh --galaxy-name test --admin 76561198023196598 --datapath ./galaxies
+/save
+/stop 
+# start server # 
+./server.sh --galaxy-name test --admin 76561198023196598 --datapath ./galaxies/ 
+
+# Ports #
+27000 - UDP & TCP
+27003 - UDP
+27020 - UDP
+27021 - UDP
