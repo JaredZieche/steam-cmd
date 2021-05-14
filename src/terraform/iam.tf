28a4245594cd5d72c4f5b2ba6d2c378616067e0b
jaredzieche <jaredzieche@gmail.com>
2021-05-14 11:39:52 -0400

    added some more terraform config

--- src/terraform/iam.tf
+++ src/terraform/iam.tf
@@ -1,9 +1,8 @@
+# steam s3 access policy for backup bucket
 resource "aws_iam_role_policy" "steam_access_policy" {
-  name = "test_policy"
+  name = "steam_s3_policy"
   role = aws_iam_role.steam_cmd_role.id
 
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -14,7 +13,7 @@ resource "aws_iam_role_policy" "steam_access_policy" {
           "s3:GetBucketObject",
         ]
         Effect   = "Allow"
-        Resource = "*"
+        Resource = "arn:aws:s3:::steamcmd*"
       },
     ]
   })
@@ -29,11 +28,45 @@ resource "aws_iam_role" "steam_cmd_role" {
       {
         Action = "sts:AssumeRole"
         Effect = "Allow"
-        Sid    = ""
+        Sid    = "${aws_iam_role_policy.steam_access_policy.id}"
         Principal = {
           Service = "ec2.amazonaws.com"
         }
       },
     ]
+  })
+}
+
+# mfa policy
+resource "aws_iam_policy" "enforce_mfa" {
+  name = "enforce_mfa"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+            "Sid": "BlockMostAccessUnlessSignedInWithMFA",
+            "Effect": "Deny",
+            "NotAction": [
+                "iam:CreateVirtualMFADevice",
+                "iam:DeleteVirtualMFADevice",
+                "iam:ListVirtualMFADevices",
+                "iam:EnableMFADevice",
+                "iam:ResyncMFADevice",
+                "iam:ListAccountAliases",
+                "iam:ListUsers",
+                "iam:ListSSHPublicKeys",
+                "iam:ListAccessKeys",
+                "iam:ListServiceSpecificCredentials",
+                "iam:ListMFADevices",
+                "iam:GetAccountSummary",
+                "sts:GetSessionToken"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "Bool": {
+                    "aws:MultiFactorAuthPresent": "false"
+                }
+            }
+      }
   })
 }
--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -2,7 +2,3 @@ provider "aws" {
     profile = "default"
     region  = "${var.aws_region}"
 }
-
-resource "aws_instance" "steam_cmd" {
-  ami   = "${var.steam_cmd_ami_latest}"
-  instance_type   = "${var.steam_cmd_instance_type}"
